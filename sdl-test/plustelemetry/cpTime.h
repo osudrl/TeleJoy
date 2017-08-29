2d3132dba1feebcb360c0a4b96b21285fe9a171e
Kevin Kellar <kellar.kevin@gmail.com>
2017-08-29 10:42:07 -0700

    began work on a plustelemtry test

---
+++ sdl-test/plustelemetry/cpTime.h
@@ -0,0 +1,6 @@
+#ifndef CPTIME_H_
+#define CPTIME_H_
+#include <stdbool.h>
+long cpMillis();
+void cpSleep(int sleepMs);
+#endif
---
+++ sdl-test/plustelemetry/sdl-example.c
@@ -0,0 +1,110 @@
+#include <math.h>
+#include <stdio.h>
+#include <SDL2/SDL.h>
+#include <string.h>
+#include <stdbool.h>
+
+
+SDL_Joystick *get_xbox_joystick()
+{
+    
+    const int n = SDL_NumJoysticks();
+    char name[] = "Teensyduino Serial/Keyboard/Mouse/Joystick";
+    for (int i = 0; i < n; ++i) {
+        if (strncmp(SDL_JoystickNameForIndex(i), name, sizeof name) == 0)
+            return SDL_JoystickOpen(i);
+    }
+    return NULL;
+}
+
+
+int main(void)
+{   
+    printf("about to init\n");
+    SDL_Init(SDL_INIT_JOYSTICK);
+    printf("has init\n");
+    SDL_Joystick *ctrl = get_xbox_joystick();
+    
+
+    double radio_input[16] = {0};
+
+
+    while(ctrl) 
+    {
+        SDL_JoystickUpdate();
+        for (int i = 0; i < 16; i++)
+        {
+                 radio_input[i] = SDL_JoystickGetAxis(ctrl, i) / 32768.0;
+                 printf("A%d:%.3f ",i,radio_input[i]);
+        }
+
+        for (int i = 0; i < 5; i++)
+        {
+            printf("D%i:%d ",i,SDL_JoystickGetButton(ctrl, i));
+        }
+        printf("\n");
+        //cpSleep(1000);        
+    }
+    SDL_Quit();
+    printf("k done\n");
+    /*
+    int motor_index = 0;
+    int mode_index = 1;
+    int hat_last = 0;
+
+    cassie_t *c = cassie_init();
+    cassie_vis_t *v = cassie_vis_init();
+    cassie_state_t *s = cassie_state_alloc();
+    cassie_get_state(c, s);
+
+    double radio_input[16] = {0};
+    radio_input[6] = 1;
+    radio_input[8] = 1;
+
+    double messages[4] = {-1, -1, -1, -1};
+
+    do {
+        int steps_per_frame = SDL_JoystickGetButton(xbox, 0) ? 3 : 33;
+
+        for (int i = 0; i < steps_per_frame; ++i)
+            cassie_step(c, radio_input);
+
+        cassie_messages(c, messages);
+        printf("%.0f, %.0f, %.0f, %.0f\n", messages[0], messages[1], messages[2], messages[3]);
+
+        if (xbox) {
+            SDL_JoystickUpdate();
+            radio_input[0] = -SDL_JoystickGetAxis(xbox, 1) / 32768.0;
+            radio_input[1] =  SDL_JoystickGetAxis(xbox, 0) / 32768.0;
+            radio_input[2] = -SDL_JoystickGetAxis(xbox, 4) / 32768.0;
+            radio_input[3] =  SDL_JoystickGetAxis(xbox, 3) / 32768.0;
+            radio_input[6] = -SDL_JoystickGetAxis(xbox, 2) / 32768.0;
+            radio_input[7] = -SDL_JoystickGetAxis(xbox, 5) / 32768.0;
+
+            int hat = SDL_JoystickGetHat(xbox, 0);
+            if (hat & 0x1 && !(hat_last & 0x01)) ++mode_index;
+            if (hat & 0x2 && !(hat_last & 0x02)) ++motor_index;
+            if (hat & 0x4 && !(hat_last & 0x04)) --mode_index;
+            if (hat & 0x8 && !(hat_last & 0x08)) --motor_index;
+            mode_index = (mode_index + 3) % 3;
+            motor_index = (motor_index + 5) % 5;
+            hat_last = hat;
+
+            if (SDL_JoystickGetButton(xbox, 7))
+                cassie_set_state(c, s);
+
+            radio_input[4] = motor_index / 4.0;
+            radio_input[9] = mode_index - 1.0;
+        }
+    } while (cassie_vis_draw(v, c));
+
+    cassie_vis_free(v);
+    cassie_free(c);
+    cassie_cleanup();
+    
+    SDL_Quit();
+    */
+
+    return 0;
+
+}
